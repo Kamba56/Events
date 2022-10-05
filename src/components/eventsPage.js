fcee9a4fc4f9d6829c4532b98db877798178a21a
Kamba56 <tomajohn316@yahoo.com>
2022-10-05 08:30:46 +0100

    Final touchees

--- src/components/eventsPage.js
+++ src/components/eventsPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import image from '../assets/Eventimage.svg';
 import icon1 from '../assets/Calendar.svg';
 import icon2 from '../assets/Location.svg';
+import icon4 from '../assets/arrow.svg';
 import '../events.css';
 
 const EventsPage = () => (
@@ -10,12 +11,12 @@ const EventsPage = () => (
       <img src={image} alt='events header' />
     </div>
 
-    <div className='left intro pad'>
+    <div className='left intro pad img-div'>
       <h1>Birthday bash</h1>
       <p>Hosted by Esta</p>
     </div>
 
-    <section className='left pad'>
+    <section className='left pad img-div'>
       <div className='domain'>
         <div className='icon1'>
           <img src={icon1} alt='calender' />
@@ -23,6 +24,22 @@ const EventsPage = () => (
         <div className='flex-column'>
           <h2 className='left'>18 August 6:00pm</h2>
           <p className='left'>to 19 August 1:00pm</p>
+        </div>
+      </div>
+
+      <div className='flex bottom'>
+        <div className='domain'>
+          <div className='icon1'>
+            <img src={icon2} alt='calender' />
+          </div>
+          <div className='flex-column'>
+            <h2 className='left'>Street name</h2>
+            <p className='left'>19 london street</p>
+          </div>
+        </div>
+
+        <div className='icon sm'>
+          <img className='in-arr' src={icon4} alt='icon' />
         </div>
       </div>
     </section>
