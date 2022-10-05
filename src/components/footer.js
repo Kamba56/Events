472d0b9bba0224a005a5e932da727b7a17070d44
Kamba56 <tomajohn316@yahoo.com>
2022-10-05 05:40:03 +0100

    Add landing page markup

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <i class="bi bi-arrow-clockwise"></i>
+    <i class="bi bi-1-circle-fill"></i>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/landingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => (
+  <main>
+    <section id='headline' className='contain'>
+      <h1>Imagine if <span>Snapchat</span> had events.</h1>
+      <p>Easily host and share events with your friends across any social media.</p>
+    </section>
+  </main>
+);
+
+export default LandingPage;
